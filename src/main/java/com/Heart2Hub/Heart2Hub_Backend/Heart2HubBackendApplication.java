757470e41c5cfae0a790d47dd8568cb53b44eef8
kohwenjie <70895902+kohwenjie@users.noreply.github.com>
2023-08-28 23:12:38 +0800

    First Commit
    Basic setup with with dependencies needed and connection to postgresql database. No other folders and files added.

---
+++ src/main/java/com/Heart2Hub/Heart2Hub_Backend/Heart2HubBackendApplication.java
@@ -0,0 +1,13 @@
+package com.Heart2Hub.Heart2Hub_Backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Heart2HubBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Heart2HubBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Heart2Hub/Heart2Hub_Backend/Heart2HubBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Heart2Hub.Heart2Hub_Backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Heart2HubBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
