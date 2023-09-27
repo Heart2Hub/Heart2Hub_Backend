6a2e1421686df919e7f93653151fe28f00760495
Elgin Chan <elginchan13@gmail.com>
2023-09-27 18:54:30 +0800

    IHI-181: Create Patient method

--- src/main/java/com/Heart2Hub/Heart2Hub_Backend/controller/PatientController.java
+++ src/main/java/com/Heart2Hub/Heart2Hub_Backend/controller/PatientController.java
@@ -1,8 +1,12 @@
 package com.Heart2Hub.Heart2Hub_Backend.controller;
 
-import com.Heart2Hub.Heart2Hub_Backend.entity.Facility;
+import com.Heart2Hub.Heart2Hub_Backend.entity.*;
 import com.Heart2Hub.Heart2Hub_Backend.service.FacilityService;
 import com.Heart2Hub.Heart2Hub_Backend.service.PatientService;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jdk8.Jdk8Module;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import com.fasterxml.jackson.module.paramnames.ParameterNamesModule;
 import lombok.RequiredArgsConstructor;
 import org.springframework.boot.configurationprocessor.json.JSONException;
 import org.springframework.boot.configurationprocessor.json.JSONObject;
@@ -17,6 +21,17 @@ import java.util.*;
 public class PatientController {
 
     private final PatientService patientService;
+
+    @PostMapping(value = "/createPatient", consumes = {"application/json"}, produces = {"application/json"})
+    public ResponseEntity<Patient> createPatient(@RequestBody Map<String, Object> requestBody) {
+        ObjectMapper objectMapper = new ObjectMapper()
+                .registerModule(new ParameterNamesModule())
+                .registerModule(new Jdk8Module())
+                .registerModule(new JavaTimeModule());
+        Patient patient = objectMapper.convertValue(requestBody.get("patient"), Patient.class);
+        ElectronicHealthRecord ehr = objectMapper.convertValue(requestBody.get("ehr"), ElectronicHealthRecord.class);
+        return ResponseEntity.ok(patientService.createPatient(patient, ehr));
+    }
 
     @GetMapping("/getAllPatientsWithElectronicHealthRecordSummaryByName")
     public ResponseEntity<List<Map<String, Object>>> getAllPatientsWithElectronicHealthRecordSummaryByName(
