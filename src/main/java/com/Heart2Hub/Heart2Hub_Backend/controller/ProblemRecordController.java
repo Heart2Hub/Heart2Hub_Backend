2fb5f08a08cfc67b3c4088f0e7d7edeba7d23865
ernestchan <ernest.chan@u.nus.edu>
2023-09-29 16:40:30 +0800

    IHIS-52 & 76: CRUD methods and endpoints for problem record and resolve

---
+++ src/main/java/com/Heart2Hub/Heart2Hub_Backend/controller/ProblemRecordController.java
@@ -0,0 +1,63 @@
+package com.Heart2Hub.Heart2Hub_Backend.controller;
+
+import com.Heart2Hub.Heart2Hub_Backend.entity.MedicalHistoryRecord;
+import com.Heart2Hub.Heart2Hub_Backend.entity.ProblemRecord;
+import com.Heart2Hub.Heart2Hub_Backend.entity.ProblemRecord;
+import com.Heart2Hub.Heart2Hub_Backend.service.ProblemRecordService;
+import com.Heart2Hub.Heart2Hub_Backend.service.ProblemRecordService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/problemRecord")
+@RequiredArgsConstructor
+public class ProblemRecordController {
+
+    private final ProblemRecordService problemRecordService;
+
+    @PostMapping("/createProblemRecord")
+    public ResponseEntity<ProblemRecord> createProblemRecord(
+            @RequestParam Long electronicHealthRecordId,
+            @RequestBody ProblemRecord newProblemRecord) {
+        return ResponseEntity.ok(
+                problemRecordService.createProblemRecord(electronicHealthRecordId,newProblemRecord)
+        );
+    }
+
+    @GetMapping("/getAllProblemRecordsByElectronicHealthRecordId")
+    public ResponseEntity<List<ProblemRecord>> getAllProblemRecordsByElectronicHealthRecordId(
+            @RequestParam("electronicHealthRecordId") Long electronicHealthRecordId) {
+        return ResponseEntity.ok(
+                problemRecordService.getAllProblemRecordsByElectronicHealthRecordId(electronicHealthRecordId)
+        );
+    }
+
+    @DeleteMapping("/deleteProblemRecord")
+    public ResponseEntity<String> deleteProblemRecord(
+            @RequestParam("electronicHealthRecordId") Long electronicHealthRecordId,
+            @RequestParam("problemRecordId") Long problemRecordId) {
+        return ResponseEntity.ok(problemRecordService.deleteProblemRecord(electronicHealthRecordId,problemRecordId)
+        );
+    }
+
+    @PutMapping("/updateProblemRecord")
+    public ResponseEntity<ProblemRecord> updateProblemRecord(
+            @RequestParam("problemRecordId") Long problemRecordId,
+            @RequestBody ProblemRecord updatedProblemRecord) {
+        return ResponseEntity.ok(problemRecordService.updateProblemRecord(problemRecordId,updatedProblemRecord)
+        );
+    }
+
+    @PostMapping("/resolveProblemRecord")
+    public ResponseEntity<MedicalHistoryRecord> resolveProblemRecord(
+            @RequestParam Long electronicHealthRecordId,
+            @RequestParam("problemRecordId") Long problemRecordId) {
+        return ResponseEntity.ok(
+                problemRecordService.resolveProblemRecord(electronicHealthRecordId,problemRecordId)
+        );
+    }
+
+}
